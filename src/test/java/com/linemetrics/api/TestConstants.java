f809df5507993ffa5140ccc9cb5e141235de4f30
Klemens Engelbrechtsmüller <K.Engelbrechtsmueller@linemetrics.com>
2017-05-11 13:52:51 +0200

    Fix - add TestConstants class

---
+++ src/test/java/com/linemetrics/api/TestConstants.java
@@ -0,0 +1,15 @@
+package com.linemetrics.api;
+
+public class TestConstants {
+
+    //TODO define params for testing
+    public static final String VALID_CLIENTID = "";
+    public static final String VALID_CLIENTSECRET = "";
+    public static final String VALID_OBJECT_ID = "";
+    public static final String VALID_OBJECT_ALIAS = "";
+    public static final String VALID_CUSTOMKEY = "";
+    public static final String VALID_OBJECTID_DATASTREAM_TOUPDATE = "";
+    public static final String VALID_OBJECTID_DATASTREAM_TOREAD = "";
+    public static final String VALID_OBJECTID_DIGITAL = "";
+    public static final String VALID_ALIAS_DIGITAL  = "";
+}
